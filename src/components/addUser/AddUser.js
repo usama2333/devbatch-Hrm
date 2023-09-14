ec0e1950c46f7b7e2df8ab8ce18d870cb2912086
usama2333 <usama.wizz@gmail.com>
2023-09-14 18:32:06 +0500

    add new user design completed

--- src/components/addUser/AddUser.js
+++ src/components/addUser/AddUser.js
@@ -1,11 +1,28 @@
 import { Box, Typography } from "@mui/material";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { inputTextStyle } from "./style";
 import SwitchButton from "../switch/SwitchButton";
 import Button from "@mui/material/Button";
+import dummy from "../../assests/images/dummy.png";
+import cancel from "../../assests/images/cancel.png";
+import download from "../../assests/images/download.png";
+import UploadIcon from "@mui/icons-material/Upload";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const AddUser = () => {
+  const [department, setDepartment] = useState("Select your department");
+  const [gender, setGender] = useState("Male");
+  const handleChangeDepartment = (event) => {
+    setDepartment(event.target.value);
+  };
+  const handleChangeGender = (event) => {
+    setGender(event.target.value);
+  };
+
   return (
     <Fragment>
       <Box
@@ -81,27 +98,110 @@ const AddUser = () => {
                 autoComplete="off"
               />
 
-              <Typography sx={inputTextStyle}>Department</Typography>
-              <TextField
-                id="outlined-basic"
+              <InputLabel sx={inputTextStyle} id="department">
+                Department
+              </InputLabel>
+              <Select
+                labelId="department"
                 fullWidth
+                // placeholder="Select a department"
                 size="small"
-                variant="outlined"
-                placeholder="Select a department"
-              />
+                id="department"
+                value={department}
+                onChange={handleChangeDepartment}
+              >
+                <MenuItem value={10}>IT</MenuItem>
+                <MenuItem value={20}>Computer Science</MenuItem>
+                <MenuItem value={30}>Engeeniring</MenuItem>
+              </Select>
 
-              <Typography sx={inputTextStyle}>Gender</Typography>
-              <TextField
-                id="outlined-basic"
+              <InputLabel sx={inputTextStyle} id="gender">
+                Gender
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
                 fullWidth
+                // placeholder="Select a department"
                 size="small"
-                variant="outlined"
-                placeholder="Select Gender"
-              />
+                id="gender"
+                value={gender}
+                onChange={handleChangeGender}
+              >
+                <MenuItem value={10}>Male</MenuItem>
+                <MenuItem value={20}>Female Science</MenuItem>
+                <MenuItem value={30}>other</MenuItem>
+              </Select>
             </Box>
 
             <Box sx={{ flex: "0 0 49%" }}>
-              <Box sx={{ height: "140px" }}></Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  pr: "3rem",
+                }}
+              >
+                <Typography sx={inputTextStyle}>Upload Image</Typography>
+                <Box
+                  sx={{
+                    background: "#FFFFFF 0% 0% no-repeat padding-box",
+                    border: "1px solid #D0D5DD",
+                    borderRadius: "10px",
+                    height: "130px",
+                    width: "130px",
+                    mt: "1rem",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    // ml : '4rem'
+                  }}
+                >
+                  <Box component="img" src={dummy}></Box>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    // ml :'3rem'
+                  }}
+                >
+                  <Button
+                    sx={{
+                      background: "#FE8062 0% 0% no-repeat padding-box",
+                      "&:hover": {
+                        backgroundColor: "#d66145",
+                      },
+                    }}
+                    variant="contained"
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                      }}
+                    >
+                      <Box>
+                        <UploadIcon />
+                      </Box>
+                      <Typography
+                        sx={{
+                          font: "normal normal normal 14px/17px Product Sans",
+                          color: "#FFFFFF",
+                          textTransform: "capitalize",
+                          ml: "0.8rem",
+                        }}
+                      >
+                        Upload
+                      </Typography>
+                    </Box>
+                  </Button>
+                </Box>
+                <Box
+                  component="img"
+                  src={cancel}
+                  sx={{ height: "22px", width: "22px", mt: "1rem" }}
+                ></Box>
+              </Box>
 
               <Typography sx={inputTextStyle}>Phone No</Typography>
               <TextField
@@ -164,9 +264,9 @@ const AddUser = () => {
               color: "#FFFFFF",
               maxWidth: "376px",
               // padding : '0 1rem',
-              pl :'3rem',
-              pr : '3rem',
-              textTransform : 'capitalize',
+              pl: "3rem",
+              pr: "3rem",
+              textTransform: "capitalize",
               background: "#FE8062 0% 0% no-repeat padding-box",
               "&:hover": {
                 backgroundColor: "#d66145",
