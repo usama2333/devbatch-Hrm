412c36ef649d13b6ddc4a4e7d0dddb6c41113137
usama2333 <usama.wizz@gmail.com>
2023-10-10 19:09:17 +0500

    handle / routing on login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Fragment, useContext} from "react";
+import React, { Fragment, useContext,} from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Redirect 
 } from "react-router-dom";
+import { Outlet,Navigate } from "react-router-dom";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Reset from './pages/reset/Reset';
@@ -14,9 +16,12 @@ import Dashboard from "./components/dashboard/Dashboard";
 import User from "./components/user/User";
 import AddUser from "./components/addUser/AddUser";
 import UserDetail from "./components/userdetail/UserDetail";
+import {useSelector } from "react-redux";
 
 function App() {
   
+  
+  const login = useSelector((state) => state.table.login);
   const authCtx = useContext(AuthContext);
 
   return (
@@ -24,7 +29,12 @@ function App() {
     <Router>
       <Routes>
        <Route element={<PrivateRoutes/>}>
-          <Route element={<Home/>} path="/" exact>
+          <Route element={<Home/>} path='/' exact>
+          
+            {login ? (
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        ) : null}
+         
              <Route element={<Dashboard/>} path="dashboard" exact/> 
              <Route element={<User/>} path="user" exact/>
              <Route element={<AddUser/>} path="adduser" exact  />
